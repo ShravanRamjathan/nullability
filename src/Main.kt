a79c2be6df305ce4553ca5f753565dc054c54109
Shravan Ramjathan <128147436+ST10247982@users.noreply.github.com>
2025-04-01 17:25:22 +0200

    Initial

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
+var favouriteActor:String? = null         // by putting the ?, it makes it nullable
+    favouriteActor = "Meryl Sreep"
+    println(favouriteActor)
 
+    var number:Int?  = null
+    println(number)
 }
